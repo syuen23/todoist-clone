7677687d7f870fad24c5704112fffbeb5d2e6703
Simon Yuen <54995458+syuen23@users.noreply.github.com>
2024-02-04 03:11:47 -0500

    Add routes from auth blueprint

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -8,13 +8,17 @@ login_manager = LoginManager()
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.Config")
 
     db.init_app(app)
     login_manager.init_app(app)
 
     with app.app_context():
+        from .routes.auth import auth_bp
+
+        app.register_blueprint(auth_bp)
+
         db.create_all()
 
         return app
