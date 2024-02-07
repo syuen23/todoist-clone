be9544b175f2f3d2ce5c62c0f157eeb1b60d01b8
Simon Yuen <54995458+syuen23@users.noreply.github.com>
2024-02-07 15:03:16 -0500

    Update Account SQLAlchemy model
    Add inheritance from Flask-Login's Usermixin class in order to support
    Flask-Login required fields and functions.
    
    Remove the username field and updated the __repr__ function to include
    the account's id and email.

--- backend/api/models/account.py
+++ backend/api/models/account.py
@@ -1,18 +1,14 @@
 from api import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
-class Account(db.Model):
+class Account(UserMixin, db.Model):
     __tablename__ = "account"
 
     id = db.Column(
         db.Integer,
         primary_key=True,
-    )
-    username = db.Column(
-        db.String(100),
-        nullable=False,
-        unique=False,
     )
     email = db.Column(
         db.String(40),
@@ -36,4 +32,4 @@ class Account(db.Model):
         return check_password_hash(self.password, password)
 
     def __repr__(self):
-        return "<User {}>".format(self.username)
+        return f"<User id={self.id}, email={self.email}>"
